a4e795733d563bec49e37fcca2fe04273352ce64
HBSPS <wiyeong79@naver.com>
2022-07-03 16:15:50 +0900

    :sparkles: Add BOJ 10816
    백준 10816 숫자 카드2

---
+++ BOJ/10816 숫자 카드2.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+M = int(input())
+B = list(map(int, input().split()))
+
+A.sort()
+
+A = deque(A)
+
+dict = {}
+
+for i in range(N):
+    tmp = A.popleft()
+
+    if tmp in dict:
+        dict[tmp] += 1
+    else:
+        dict[tmp] = 1
+
+print(dict)
+
+ans = []
+
+for i in B:
+    if i in dict:
+        ans.append(dict[i])
+    else:
+        ans.append(0)
+
+for i in ans:
+    print(i, end=" ")
