0845db48c3f5fa1182a7bca4ecaf6f056326dc33
HBSPS <wiyeong79@naver.com>
2023-02-20 22:57:45 +0900

    :sparkles: Add BOJ 2557 JAVA
    백준 2557 Hello World JAVA

---
+++ BOJ/기타/입출력/2557 Hello World/Main.java
@@ -0,0 +1,11 @@
+// 2557 Hello World
+
+// JAVA의 기본 입출력 연습
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
